2704a5cc16890e0a77709550302a90f750ebf8f8
votrunghau11 <vth1122@gmail.com>
2016-11-07 22:13:33 +0700

    fix #16

--- PongGame1/src/vn/vanlanguni/ponggame/PongPanel.java
+++ PongGame1/src/vn/vanlanguni/ponggame/PongPanel.java
@@ -254,7 +254,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 
 			// Draw scores
 			// TODO Set Blue color
-			g.setColor(Color.BLUE);
+			g.setColor(Color.blue);
 			g.setFont(new Font(Font.DIALOG, Font.BOLD, 36));
 			g.drawString(String.valueOf(playerOneScore), 100, 100);
 			g.drawString(String.valueOf(playerTwoScore), 400, 100);
