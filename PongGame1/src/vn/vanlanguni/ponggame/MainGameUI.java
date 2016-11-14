520b17bc07164f75a3ec2e2aebaabcdb55208e42
votrunghau11 <vth1122@gmail.com>
2016-11-14 15:19:13 +0700

    fix #33

--- PongGame1/src/vn/vanlanguni/ponggame/MainGameUI.java
+++ PongGame1/src/vn/vanlanguni/ponggame/MainGameUI.java
@@ -32,7 +32,7 @@ public class MainGameUI extends JFrame{
     public static void main(String[] args) {
        MainGameUI mainFrame = new MainGameUI();
        mainFrame.setVisible(true);
-       //mainFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+       mainFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
        
     }
 }
